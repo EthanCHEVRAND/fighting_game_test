2240d21b6bd7e8fffd1927fbb35e7aafda25e890
EthanCHEVRAND <ethan.chevrand@edu.univ-fcomte.fr>
2025-03-19 23:58:28 +0100

    initial Archer.java commit

--- Archer.java
+++ Archer.java
@@ -1,3 +1,23 @@
+import java.util.Scanner;
+
 public class Archer extends Character {
-    
+    private int nbMaxArrow ;
+    private int nbArrow ;
+
+
+    public Archer() {
+        super() ;
+    }
+
+    @Override
+    public void init() {
+        super.init() ;
+
+        Scanner scan = new Scanner(System.in) ;
+        System.out.print("Enter the maximum amount of arrows : ");
+        this.nbMaxArrow = scan.nextInt() ;
+        this.nbArrow = this.nbMaxArrow ;
+        System.out.println("-------------------- Archer created --------------------");
+    }
+
 }
