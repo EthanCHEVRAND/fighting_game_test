b23c423600ece543cdfe7ba4285f27ed87943224
EthanCHEVRAND <ethan.chevrand@edu.univ-fcomte.fr>
2025-03-20 18:58:39 +0100

    added all the characteristics to the setters and refactored the init()

--- Tank.java
+++ Tank.java
@@ -6,7 +6,6 @@ import java.util.Scanner;
  * @author ZeSpatule
  */
 public class Tank extends Character {
-    // TODO add the requirements to setters and init
     /*
      * Characteristics : 
      * High hp amount (175-200) with very low luck stat (0-0.2)
@@ -22,19 +21,9 @@ public class Tank extends Character {
     public Tank() {
     }
 
-    @Override
-    public void init() {
-        super.init() ;
-
-        @SuppressWarnings({ "ressource", "resource" })
-        Scanner scan = new Scanner(System.in) ;
-
-        System.out.print("Enter the maximum possible shield value : ");
-        this.maxShield = scan.nextInt() ;
-        this.shield = 0 ;
-        System.out.println("--------------------- Tank created ---------------------\n");
-    }
-
+    /**
+     * Sets base health to proper value
+     */
     @Override
     public void setBaseHealth(int bhp) {
         if (bhp < 175) {
@@ -46,10 +35,79 @@ public class Tank extends Character {
         }
     }
 
-    public void setMaxShield(int value) {
-        this.maxShield = value ;
+    /**
+     * Sets strength to the proper value
+     */
+    @Override
+    public void setStrength(int str) {
+        if (str < 35) {
+            super.setStrength(35);
+        } else if (str > 40) {
+            super.setStrength(40);
+        } else {
+            super.setStrength(str);
+        }
     }
 
+    /**
+     * Sets power to proper value
+     */
+    @Override
+    public void setPower(int pow) {
+        if (pow < 30) {
+            super.setPower(30);
+        } else if (pow > 35) {
+            super.setStrength(35);
+        } else {
+            super.setStrength(pow);
+        }
+    }
+
+    /**
+     * Sets defense to proper value
+     */
+    @Override
+    public void setDefense(int def) {
+        if (def < 20) {
+            super.setDefense(20) ;
+        } else if (def > 25) {
+            super.setDefense(25) ;
+        } else {
+            super.setDefense(def) ;
+        }
+    }
+
+    /**
+     * Sets special defense to proper value
+     */
+    @Override
+    public void setSpecialDefense(int spe_def) {
+        if (spe_def < 10) {
+            super.setSpecialDefense(10);
+        } if (spe_def > 15) {
+            super.setSpecialDefense(15);
+        } else {
+            super.setSpecialDefense(spe_def);
+        }
+    }
+
+    /**
+     * Sets max shield to proper value
+     * @param value
+     */
+    public void setMaxShield(int value) {
+        if (value < 30)
+            this.maxShield = 30 ;
+        else if (value > 50)
+            this.maxShield = 50 ;
+        else 
+            this.maxShield = value ;
+    }
+
+    /**
+     * Sets the shield to the value
+     * @param value
+     */
     public void setShield(int value) {
         if (value <= this.maxShield)
             this.shield = value ;
@@ -57,16 +115,44 @@ public class Tank extends Character {
             this.shield = this.maxShield ;
     }
 
+    /**
+     * returns the max amount of shield
+     * @return this.maxShield
+     */
     public int getMaxShield() {
         return this.maxShield ;
     }
 
+    /**
+     * Returns the current amount of shield
+     * @return this.shield
+     */
     public int getShield() {
         return this.shield ;
     }
 
+    @Override
+    public void init() {
+        System.out.println("Hp (175-200) | Skill points (30-45)\nStrength (35-40) | Power (30-35)\nDefense (20-25) | Special defense (10-15)\nMax shield (30-50)");
+        super.init() ;
 
+        @SuppressWarnings({ "ressource", "resource" })
+        Scanner scan = new Scanner(System.in) ;
 
+        System.out.print("Enter the maximum possible shield value : ");
+        this.setMaxShield(scan.nextInt()) ;
+        this.shield = 0 ;
+
+        // makes the stats work properly with the class
+        this.setBaseHealth(this.getBaseHealth());
+        this.setStrength(this.getStrength());
+        this.setPower(this.getPower());
+        this.setDefense(this.getPower());
+        this.setSpecialDefense(this.getSpecialDefense());
+        System.out.println("--------------------- Tank created ---------------------\n");
+    }
+
+    
     public boolean activateShield() {
         if (this.getSkillPoints() >= 15) {
             this.setShield((this.getPower() * this.getDefense()) / 25) ; 
