4a9e54d4b4aa3935b23ec421e8efff6db4680a1a
EthanCHEVRAND <ethan.chevrand@edu.univ-fcomte.fr>
2025-03-20 22:29:58 +0100

    added Mage and Element class

---
+++ Element.java
@@ -0,0 +1,11 @@
+/**
+ * Management of the elements for a mage
+ * @author ZeSpatule
+ */
+public class Element {
+    public String type ;
+
+    public Element(String typeE) {
+        this.type = typeE ;
+    }
+}
---
+++ Mage.java
@@ -0,0 +1,18 @@
+
+/**
+ * Character's subclass
+ * Managing class for Mage ingame class
+ * @author ZeSpatule
+ */
+public class Mage extends Character {
+    /*
+     * Characteristics : 
+     * Low hp amount (90-120) with moderate luck stat (0.10-0.20)
+     * sp -> 60
+     * Moderate strength (35-40) with high power (40-50)
+     * Low defense (5-10) with moderate special_defense (10-15)
+     * Element
+     */
+
+     private Element element ;
+}
